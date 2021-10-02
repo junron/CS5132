c4ffd1b69ffa8b8c6377d9e86c1705d3fc22766e
Lam Jun Rong <junron1@outlook.com>
2021-10-02 16:50:42 +0800

    Cleanup

--- src/main/java/pa3/KDTreeTester2.java
+++ src/main/java/pa3/KDTreeTester2.java
@@ -2,19 +2,21 @@ package pa3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class KDTreeTester2 {
-  public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws IOException {
     long build = System.currentTimeMillis();
     KDTree kdtree = new KDTree("ATMLocations.csv");
     Scanner scanner = new Scanner(new File("target-locations.csv"));
-    Scanner scanner2 = new Scanner(new File("output.txt"));
+    FileWriter scanner2 = new FileWriter(new File("output.txt"));
     int numLocations = Integer.parseInt(scanner.nextLine());
 
     long start = System.currentTimeMillis();
     System.out.println("Build took " + (start - build) + " milliseconds");
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < numLocations; i++) {
       String input = scanner.nextLine();
       String[] parts = input.split(",");
       double x = Double.parseDouble(parts[2]);
@@ -26,7 +28,8 @@ public class KDTreeTester2 {
       // if (!resStr.equals(expected)) {
       //   throw new AssertionError(resStr + " != " + expected);
       // }
-      System.out.println(resStr);
+      scanner2.write(resStr + "\n");
+      // System.out.println(resStr);
     }
     long end = System.currentTimeMillis();
     System.out.println("Operation took " + (end - start) + " milliseconds");
